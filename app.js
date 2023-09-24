2672fff70d2475c969466dafb3562dc9c3deb579
Ricki Beckless <glbeckless@hotmail.com>
2023-09-24 13:50:39 -0500

    added script and dropdown

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
-
 app.use(express.static("./public"));
-
-// app.use("/");
 
 app.listen(8080, () => {
     console.log("Eng.Comp.Port App now listening on port 8080");

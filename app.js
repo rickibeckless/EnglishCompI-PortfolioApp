bebdb5de29f20e6f7a591577d97ff4b00979a476
Ricki Beckless <glbeckless@hotmail.com>
2023-09-27 16:33:16 -0500

    index url change

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import express from "express";
+import path from "path";
+import url from 'url';
 
 const app = express();
+const port = 8080;
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static(publicPath));
 
-app.listen(8080, () => {
-    console.log("Eng.Comp.Port App now listening on port 8080");
+// ↓↓ if all goes to shit RETREAT! ↓↓
+// app.use(express.static("./public"));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(publicPath, "index.html"))
+})
+
+
+app.listen(port, () => {
+    console.log("Eng.Comp.Port App now listening on port " + port);
 });
---
+++ public/script/urlScript/indexURLScript.js
@@ -0,0 +1,15 @@
+// home address
+document.addEventListener('DOMContentLoaded', () => {
+    navigateToHome();
+
+    const homeLink = document.getElementById('home-link');
+    if (homeLink) {
+        homeLink.addEventListener('click', (e) => {
+            navigateToHome();
+        });
+    }
+});
+
+function navigateToHome() {
+    history.pushState(null, null, '/home');
+}

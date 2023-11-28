0ec9247985ce3de75dad7dcf961fe8340b8b4fb3
Ricki Beckless <glbeckless@hotmail.com>
2023-11-27 18:03:32 -0600

    feat: Add accordion menu, functionality, and styling to about.html

--- public/script/aboutScript/aboutScript.js
+++ public/script/aboutScript/aboutScript.js
@@ -1,1 +1,23 @@
-/* ACCORDION SCRIPTS */
+/* ACCORDION FUNCTION SCRIPT */
+const accordionHeaders = document.querySelectorAll('.content-header');
+const accordionContents = document.querySelectorAll('.content-text');
+const accordionArrows = document.querySelectorAll('.a-arrow');
+
+function showContent(content) {
+    const cTarget = document.querySelector(content.dataset.contentTarget);
+    const arrow = content.querySelector('.a-arrow');
+
+    accordionContents.forEach(accordionContent => accordionContent.classList.remove('active'));
+    accordionHeaders.forEach(content => content.classList.remove('active'));
+    accordionArrows .forEach(accordionArrow => accordionArrow.classList.remove('active'));
+
+    content.classList.add('active');
+    cTarget.classList.add('active');
+    arrow.classList.add('active');
+}
+
+accordionHeaders.forEach(content => {
+    content.addEventListener('click', () => {
+        showContent(content);
+    });
+});

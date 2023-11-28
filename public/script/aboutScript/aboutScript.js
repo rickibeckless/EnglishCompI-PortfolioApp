f3d1538ab0b5ad111bbd1b22e4e476776cf67590
Ricki Beckless <glbeckless@hotmail.com>
2023-11-28 02:45:23 -0600

    feat: add styling and functionality to about.html

--- public/script/aboutScript/aboutScript.js
+++ public/script/aboutScript/aboutScript.js
@@ -1,35 +1,44 @@
 /* ACCORDION FUNCTION SCRIPT */
-const accordionHeaders = document.querySelectorAll('.content-header');
-const accordionContents = document.querySelectorAll('.content-text');
-const accordionArrows = document.querySelectorAll('.a-arrow');
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionHeaders = document.querySelectorAll('.content-header');
+    const accordionContents = document.querySelectorAll('.content-text');
+    const accordionArrows = document.querySelectorAll('.a-arrow');
 
-let previousHeader = null;
+    let previousHeader = null;
 
-function showContent(content) {
-    const cTarget = document.querySelector(content.dataset.contentTarget);
-    const arrow = content.querySelector('.a-arrow');
+    function showContent(content) {
+        const cTarget = document.querySelector(content.dataset.contentTarget);
+        const arrow = content.querySelector('.a-arrow');
 
-    if (content === previousHeader) {
-        cTarget.classList.toggle('active');
-        content.classList.toggle('active');
-        arrow.classList.toggle('active');
-    } else {
-        if (previousHeader) {
-            document.querySelector(previousHeader.dataset.contentTarget).classList.remove('active');
+        if (content === previousHeader) {
+            cTarget.classList.toggle('active');
+            content.classList.toggle('active');
+            arrow.classList.toggle('active');
+        } else {
+            const isNestedHeader = content.closest('.nested-accordion');
+
+            if (!isNestedHeader) {
+                if (previousHeader) {
+                    document.querySelector(previousHeader.dataset.contentTarget).classList.remove('active');
+                }
+
+                [accordionContents, accordionHeaders, accordionArrows].forEach(collection =>
+                    collection.forEach(item => item.classList.remove('active'))
+                );
+            }
+
+            [cTarget, content, arrow].forEach(item => item.classList.add('active'));
+
+            previousHeader = content;
         }
-
-        [accordionContents, accordionHeaders, accordionArrows].forEach(collection =>
-            collection.forEach(item => item.classList.remove('active'))
-        );
-
-        [cTarget, content, arrow].forEach(item => item.classList.add('active'));
-
-        previousHeader = content;
     }
-}
 
-accordionHeaders.forEach(content => {
-    content.addEventListener('click', () => {
-        showContent(content);
+    accordionHeaders.forEach(content => {
+        content.addEventListener('click', () => {
+            showContent(content);
+        });
     });
+
+    // Trigger click event for the first accordion header on load
+    accordionHeaders[0].click();
 });

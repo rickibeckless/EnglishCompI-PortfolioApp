734900d96680656d537e9eabe2dc0cb0905ab71d
Ricki Beckless <glbeckless@hotmail.com>
2023-11-27 22:18:41 -0600

    feat: Add functionality to accordion in about.html

--- public/script/aboutScript/aboutScript.js
+++ public/script/aboutScript/aboutScript.js
@@ -3,17 +3,29 @@ const accordionHeaders = document.querySelectorAll('.content-header');
 const accordionContents = document.querySelectorAll('.content-text');
 const accordionArrows = document.querySelectorAll('.a-arrow');
 
+let previousHeader = null;
+
 function showContent(content) {
     const cTarget = document.querySelector(content.dataset.contentTarget);
     const arrow = content.querySelector('.a-arrow');
 
-    accordionContents.forEach(accordionContent => accordionContent.classList.remove('active'));
-    accordionHeaders.forEach(content => content.classList.remove('active'));
-    accordionArrows .forEach(accordionArrow => accordionArrow.classList.remove('active'));
+    if (content === previousHeader) {
+        cTarget.classList.toggle('active');
+        content.classList.toggle('active');
+        arrow.classList.toggle('active');
+    } else {
+        if (previousHeader) {
+            document.querySelector(previousHeader.dataset.contentTarget).classList.remove('active');
+        }
 
-    content.classList.add('active');
-    cTarget.classList.add('active');
-    arrow.classList.add('active');
+        [accordionContents, accordionHeaders, accordionArrows].forEach(collection =>
+            collection.forEach(item => item.classList.remove('active'))
+        );
+
+        [cTarget, content, arrow].forEach(item => item.classList.add('active'));
+
+        previousHeader = content;
+    }
 }
 
 accordionHeaders.forEach(content => {
